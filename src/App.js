e4f8a8e17555116d4c3b108b8f2ce3e522d487ee
Mynul Islam <mynul.sakil@gmail.com>
2024-02-12 16:29:46 +0600

    finished user added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    fetch("https://randomuser.me/api/?page=1&results=1&seed=abc")
+      .then((res) => res.json())
+      .then((data) => {
+        const newData = data.results[0];
+        let newObj = {};
+        newObj['gender']=newData.gender;
+        newObj['phone']=newData.phone;
+        newObj['email']=newData.email;
+        newObj['name']=`${newData.name.title}. ${newData.name.first} ${newData.name.last}`;
+        newObj['location']=newData.location;
+        console.log(newObj)
+        setUser(newObj)
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="pt-16 bg-blueGray-50">
+      <div className="w-full lg:w-4/12 px-4 mx-auto py-10">
+        <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg mt-16">
+          <div className="px-6 py-10">
+            <div className="flex flex-wrap justify-center">
+              <div className="w-full px-4 flex justify-center">
+                <div className="relative">
+                  <img
+                    alt="..."
+                    src="https://randomuser.me/api/portraits/women/88.jpg"
+                    className="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-150-px"
+                  />
+                </div>
+              </div>
+            
+            </div>
+            <div className="text-center mt-12 pt-12">
+              <h3 className="text-xl font-semibold leading-normal mb-2 text-blueGray-700 mb-2">
+                {user.name}
+              </h3>
+              <div className="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase">
+                <i className="fas fa-map-marker-alt mr-2 text-lg text-blueGray-400" />
+                {user.location.city} , {user.location.country}
+              </div>
+              <div className="mb-2 text-blueGray-600 mt-10">
+                <i className="fas fa fa-phone mr-2 text-lg text-blueGray-400" />
+              {user.phone}
+              </div>    
+              <div className="mb-2 text-blueGray-600 ">
+                <i className="fas fa-envelope mr-2 text-lg text-blueGray-400" />
+                {user.email} 
+              </div>
+          
+            </div>
+      
+          </div>
+        </div>
+      </div>
+ 
+    </section>
   );
 }
 
